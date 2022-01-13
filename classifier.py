9259b95af621d4453635c16493846f3d213e5a43
dltmdcks98 <dltmdcks98@gmail.com>
2022-01-13 16:50:12 +0900

    이미지 구분 성공(단 정확도 낮음)

--- classifier.py
+++ classifier.py
@@ -6,27 +6,58 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
 #파일 선택 창 생성
+
+#IU
 Tk().withdraw()
 IU_file = askopenfilename()
 print(IU_file)
+
+#TEST
+Tk().withdraw()
+IU_test = askopenfilename()
+print(IU_test)
+
+#BAEK
+Tk().withdraw()
+BAEK_file = askopenfilename()
+print(BAEK_file)
+
 
 #이미지 출력
 imgIU = face_recognition.load_image_file(IU_file)
 imgIU = cv2.cvtColor(imgIU, cv2.COLOR_BGR2RGB)
 
-Tk().withdraw()
-BAEK_file = askopenfilename()
-print(BAEK_file)
+imgTEST = face_recognition.load_image_file(IU_test)
+imgTEST = cv2.cvtColor(imgTEST,cv2.COLOR_BGR2RGB)
 
 imgBAEK = face_recognition.load_image_file(BAEK_file)
 imgBAEK = cv2.cvtColor(imgBAEK, cv2.COLOR_BGR2RGB)
 
 
-#얼굴 인식
-faceLoc = face_recognition.face_locations(imgIU)[0]
-encodeElon = face_recognition.face_encodings(imgIU)[0]
-cv2.rectangle(imgIU, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
+#=======이미지 분석
+#IU
+faceLocIU = face_recognition.face_locations(imgIU)[0]
+encodeIU = face_recognition.face_encodings(imgIU)[0]
+cv2.rectangle(imgIU, (faceLocIU[3], faceLocIU[0]), (faceLocIU[1], faceLocIU[2]), (255, 0, 255), 2)
+
+#IU_TEST 
+faceLocTEST = face_recognition.face_locations(imgTEST)[0]
+endcodeTEST = face_recognition.face_encodings(imgTEST)[0]
+cv2.rectangle(imgTEST,(faceLocTEST[3], faceLocTEST[0]), (faceLocTEST[1], faceLocTEST[2]), (255, 0, 255), 2)
+
+#BAEK
+faceLocBAEK = face_recognition.face_locations(imgBAEK)[0]
+encodeBEAK = face_recognition.face_encodings(imgBAEK)[0]
+cv2.rectangle(imgBAEK,(faceLocBAEK[3], faceLocBAEK[0]), (faceLocBAEK[1], faceLocBAEK[2]), (255, 0, 255), 2)
+
+
+#얼굴 비교 /IU
+results1 = face_recognition.compare_faces([encodeIU], endcodeTEST)
+results2 = face_recognition.compare_faces([encodeIU], encodeBEAK)
+print('IU + IU_TEST : ', results1)
+print('IU + BAEK : ', results2)
 
 cv2.imshow('IU', imgIU)
+cv2.imshow('IU_TEST',imgTEST)
 cv2.imshow('BAEK', imgBAEK)
 cv2.waitKey(0)
