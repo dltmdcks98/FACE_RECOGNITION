66edfeecaec59557f2142c5f0099ec9b2ab8310c
dltmdcks98 <dltmdcks98@gmail.com>
2022-01-13 13:58:19 +0900

    연관 강의 추가

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 Reference:
 1. https://hslee09.medium.com/python-cnn%EC%9D%84-%ED%99%9C%EC%9A%A9%ED%95%9C-%EC%97%B0%EC%98%88%EC%9D%B8-%EC%82%AC%EC%A7%84-%EB%B6%84%EB%A5%98-1-705aee34def4
 2. https://ukayzm.github.io/unknown-face-classifier/#%EC%96%BC%EA%B5%B4-%EC%9D%B8%EC%8B%9D
+3.https://wiserloner.tistory.com/1123
+4.https://www.youtube.com/watch?v=sz25xxF_AVE
 
 
 
--- classifier.py
+++ classifier.py
@@ -1,13 +1,16 @@
 #이미지 처리를 위함
-from dataclasses import replace
 import cv2
 import numpy as np
+import face_recognition
+
 #시각화를 위함
 from matplotlib import pyplot as plt
 
 #파일 선택 창을 위함
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
+
+import warnings
 
 #파일 선택 창 생성
 Tk().withdraw()
@@ -17,11 +20,25 @@ print(file)
 #이미지 불러오기
 image = cv2.imread(file)
 
+plt.figure(figsize=(12,8))
+plt.imshow(image)
+plt.xticks([]), plt.yticks([]) #x,y측 숨김
+plt.show
+
 #흑백으로 전환
 grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+plt.imshow(grayImage)
+print("change succes")
 
 #흑백 이미지 그리기 
 plt.figure(figsize=(12,8))
 plt.imshow(grayImage, cmap='gray')
 plt.xticks([]), plt.yticks([]) #x,y측 숨김
 plt.show
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+faces = face_cascade.detectMultiScale(grayImage, 1.3, 5)
+print(faces.shape)
+print("face: " + str(faces.shape[0]))
+
