c399a9ffd001252a5d82c466cca5eff4d758e965
dltmdcks98 <dltmdcks98@gmail.com>
2022-01-13 15:16:49 +0900

    IU_TEST이미지의 얼굴 인식 확인

--- classifier.py
+++ classifier.py
@@ -1,44 +1,17 @@
-#이미지 처리를 위함
 import cv2
 import numpy as np
 import face_recognition
 
-#시각화를 위함
-from matplotlib import pyplot as plt
+imgElon = face_recognition.load_image_file('ImageBasic/IU_TEST.jpg')
+imgElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
 
-#파일 선택 창을 위함
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
+imgBill = face_recognition.load_image_file('ImageBasic/BAEK.jpg')
+imgBill = cv2.cvtColor(imgBill, cv2.COLOR_BGR2RGB)
 
-import warnings
+faceLoc = face_recognition.face_locations(imgElon)[0]
+encodeElon = face_recognition.face_encodings(imgElon)[0]
+cv2.rectangle(imgElon, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
 
-#파일 선택 창 생성
-Tk().withdraw()
-file = askopenfilename()
-print(file)
-
-#이미지 불러오기
-image = cv2.imread(file)
-
-plt.figure(figsize=(12,8))
-plt.imshow(image)
-plt.xticks([]), plt.yticks([]) #x,y측 숨김
-plt.show
-
-#흑백으로 전환
-grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-plt.imshow(grayImage)
-print("change succes")
-
-#흑백 이미지 그리기 
-plt.figure(figsize=(12,8))
-plt.imshow(grayImage, cmap='gray')
-plt.xticks([]), plt.yticks([]) #x,y측 숨김
-plt.show
-
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-faces = face_cascade.detectMultiScale(grayImage, 1.3, 5)
-print(faces.shape)
-print("face: " + str(faces.shape[0]))
-
+cv2.imshow('Elon Musk', imgElon)
+cv2.imshow('Bill Gates', imgBill)
+cv2.waitKey(0)
