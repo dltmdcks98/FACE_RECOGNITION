a4f06a444195e7eee9a3b032f6331c68a2411378
dltmdcks98 <dltmdcks98@gmail.com>
2022-01-13 17:15:30 +0900

    기본 지식 학습 및 구현

--- classifier.py
+++ classifier.py
@@ -2,62 +2,69 @@ import cv2
 import numpy as np
 import face_recognition
 
+
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
 #파일 선택 창 생성
 
-#IU
+#Elon
 Tk().withdraw()
-IU_file = askopenfilename()
-print(IU_file)
+Elon_file = askopenfilename()
+print(Elon_file)
 
 #TEST
 Tk().withdraw()
-IU_test = askopenfilename()
-print(IU_test)
+Elon_test = askopenfilename()
+print(Elon_test)
 
-#BAEK
+#Bill
 Tk().withdraw()
-BAEK_file = askopenfilename()
-print(BAEK_file)
+Bill_file = askopenfilename()
+print(Bill_file)
 
 
 #이미지 출력
-imgIU = face_recognition.load_image_file(IU_file)
-imgIU = cv2.cvtColor(imgIU, cv2.COLOR_BGR2RGB)
+imgElon = face_recognition.load_image_file(Elon_file)
+imgElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2GRAY)
 
-imgTEST = face_recognition.load_image_file(IU_test)
+imgTEST = face_recognition.load_image_file(Elon_test)
 imgTEST = cv2.cvtColor(imgTEST,cv2.COLOR_BGR2RGB)
 
-imgBAEK = face_recognition.load_image_file(BAEK_file)
-imgBAEK = cv2.cvtColor(imgBAEK, cv2.COLOR_BGR2RGB)
+imgBill = face_recognition.load_image_file(Bill_file)
+imgBill = cv2.cvtColor(imgBill, cv2.COLOR_BGR2RGB)
 
 
 #=======이미지 분석
-#IU
-faceLocIU = face_recognition.face_locations(imgIU)[0]
-encodeIU = face_recognition.face_encodings(imgIU)[0]
-cv2.rectangle(imgIU, (faceLocIU[3], faceLocIU[0]), (faceLocIU[1], faceLocIU[2]), (255, 0, 255), 2)
+#Elon
+faceLocElon = face_recognition.face_locations(imgElon)[0]
+encodeElon = face_recognition.face_encodings(imgElon)[0]
+cv2.rectangle(imgElon, (faceLocElon[3], faceLocElon[0]), (faceLocElon[1], faceLocElon[2]), (255, 0, 255), 2)
 
-#IU_TEST 
+#Elon_TEST 
 faceLocTEST = face_recognition.face_locations(imgTEST)[0]
-endcodeTEST = face_recognition.face_encodings(imgTEST)[0]
+encodeTEST = face_recognition.face_encodings(imgTEST)[0]
 cv2.rectangle(imgTEST,(faceLocTEST[3], faceLocTEST[0]), (faceLocTEST[1], faceLocTEST[2]), (255, 0, 255), 2)
 
-#BAEK
-faceLocBAEK = face_recognition.face_locations(imgBAEK)[0]
-encodeBEAK = face_recognition.face_encodings(imgBAEK)[0]
-cv2.rectangle(imgBAEK,(faceLocBAEK[3], faceLocBAEK[0]), (faceLocBAEK[1], faceLocBAEK[2]), (255, 0, 255), 2)
+#Bill
+faceLocBill = face_recognition.face_locations(imgBill)[0]
+encodeBill = face_recognition.face_encodings(imgBill)[0]
+cv2.rectangle(imgBill,(faceLocBill[3], faceLocBill[0]), (faceLocBill[1], faceLocBill[2]), (255, 0, 255), 2)
 
 
-#얼굴 비교 /IU
-results1 = face_recognition.compare_faces([encodeIU], endcodeTEST)
-results2 = face_recognition.compare_faces([encodeIU], encodeBEAK)
-print('IU + IU_TEST : ', results1)
-print('IU + BAEK : ', results2)
+#얼굴 비교 /Elon
+results1 = face_recognition.compare_faces([encodeElon], encodeTEST)
+results2 = face_recognition.compare_faces([encodeElon], encodeBill)
+print('Elon + Elon_TEST : ', results1)
+print('Elon + Bill : ', results2)
 
-cv2.imshow('IU', imgIU)
-cv2.imshow('IU_TEST',imgTEST)
-cv2.imshow('BAEK', imgBAEK)
+faceDis1 = face_recognition.face_distance([encodeElon], encodeTEST)
+faceDis2 = face_recognition.face_distance([encodeElon], encodeBill)
+
+print('Elon + Elon_TEST : %s (%f%%)' %(results1, 1-faceDis1))
+print('Elon + Bill : %s (%f%%)' %(results2, 1-faceDis2))
+
+cv2.imshow('Elon', imgElon)
+cv2.imshow('Elon_TEST',imgTEST)
+cv2.imshow('Bill', imgBill)
 cv2.waitKey(0)
