8b72ebdba02af599f94f79002ec0e050f15591e6
dltmdcks98 <dltmdcks98@gmail.com>
2022-01-13 15:30:43 +0900

    각 인물별 선택창 출력 후 출력

--- classifier.py
+++ classifier.py
@@ -2,16 +2,31 @@ import cv2
 import numpy as np
 import face_recognition
 
-imgElon = face_recognition.load_image_file('ImageBasic/IU_TEST.jpg')
-imgElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
-imgBill = face_recognition.load_image_file('ImageBasic/BAEK.jpg')
-imgBill = cv2.cvtColor(imgBill, cv2.COLOR_BGR2RGB)
+#파일 선택 창 생성
+Tk().withdraw()
+IU_file = askopenfilename()
+print(IU_file)
 
-faceLoc = face_recognition.face_locations(imgElon)[0]
-encodeElon = face_recognition.face_encodings(imgElon)[0]
-cv2.rectangle(imgElon, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
+#이미지 출력
+imgIU = face_recognition.load_image_file(IU_file)
+imgIU = cv2.cvtColor(imgIU, cv2.COLOR_BGR2RGB)
 
-cv2.imshow('Elon Musk', imgElon)
-cv2.imshow('Bill Gates', imgBill)
+Tk().withdraw()
+BAEK_file = askopenfilename()
+print(BAEK_file)
+
+imgBAEK = face_recognition.load_image_file(BAEK_file)
+imgBAEK = cv2.cvtColor(imgBAEK, cv2.COLOR_BGR2RGB)
+
+
+#얼굴 인식
+faceLoc = face_recognition.face_locations(imgIU)[0]
+encodeElon = face_recognition.face_encodings(imgIU)[0]
+cv2.rectangle(imgIU, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
+
+cv2.imshow('IU', imgIU)
+cv2.imshow('BAEK', imgBAEK)
 cv2.waitKey(0)
