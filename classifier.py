21d7165bbd3e51d388f6fceb02a220e492a39a4f
dltmdcks98 <dltmdcks98@gmail.com>
2022-01-13 12:16:56 +0900

    이미지 흑백화_이미지 선택창 추가

--- classifier.py
+++ classifier.py
@@ -1,11 +1,21 @@
 #이미지 처리를 위함
+from dataclasses import replace
 import cv2
 import numpy as np
 #시각화를 위함
 from matplotlib import pyplot as plt
 
+#파일 선택 창을 위함
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+#파일 선택 창 생성
+Tk().withdraw()
+file = askopenfilename()
+print(file)
+
 #이미지 불러오기
-image = cv2.imread("C:/Users/dltmd\Downloads/IU_TEST/[1752705] DSC_4979.jpg")
+image = cv2.imread(file)
 
 #흑백으로 전환
 grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
