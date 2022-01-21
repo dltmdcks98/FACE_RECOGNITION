5d1a1768bad4f98fceb463b44352efa87d8d7d6f
dltmdcks98 <dltmdcks98@gmail.com>
2022-01-21 10:54:10 +0900

    정리

--- face_recognition_knn.py
+++ face_recognition_knn.py
@@ -58,20 +58,7 @@ def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree
 
 
 def predict(X_img_path, knn_clf=None, model_path=None, distance_threshold=0.6):
-    """
-    Recognizes faces in given image using a trained KNN classifier
-
-    :param X_img_path: path to image to be recognized
-    :param knn_clf: (optional) a knn classifier object. if not specified, model_save_path must be specified.
-    :param model_path: (optional) path to a pickled knn classifier. if not specified, model_save_path must be knn_clf.
-    :param distance_threshold: (optional) distance threshold for face classification. the larger it is, the more chance
-           of mis-classifying an unknown person as a known one.
-    :return: a list of names and face locations for the recognized faces in the image: [(name, bounding box), ...].
-        For faces of unrecognized persons, the name 'unknown' will be returned.
-    """
-    # if not os.path.isfile(X_img_path) or os.path.splitext(X_img_path)[1][1:] not in ALLOWED_EXTENSIONS:
-    #     raise Exception("Invalid image path: {}".format(X_img_path))
-
+   
     if knn_clf is None and model_path is None:
         raise Exception("Must supply knn classifier either thourgh knn_clf or model_path")
 
